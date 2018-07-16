3793e0866caee5e5fb6b8be03e0be14e7e2e3360
Siva R <sivagrenganathan+github@protonmail.com>
2018-07-16 10:08:08 +0530

    Update build script

--- build_prod.sh
+++ build_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/home/sivasama/wcfl-frontend-design/node_modules/@angular/cli/bin/ng build --prod
-sudo cp -r /home/sivasama/wcfl-frontend-design/dist/wcfl/* /var/www/html/
+/path/to/ng build --prod
+sudo cp -r /path/to/dist/* /var/www/html/
 sudo nginx -t
